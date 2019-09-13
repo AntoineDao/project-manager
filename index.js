6ffcd38e75fd2807c67c930d090fbe419c947a73
AntoineDao <antoine@mallzee.com>
2019-09-13 14:39:32 +0800

    feat(*): initial commit
    basic project manager functionality to react to github issue/pull request/label events

---
+++ index.js
@@ -0,0 +1,22 @@
+const handlers = require('./lib/handlers');
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  // Get an express router to expose new HTTP endpoints
+  const router = app.route('/project-manager')
+  // Use any middleware
+  router.use(require('express').static('public'))
+
+  app.on(['issues.opened', 'pull_request.opened'], handlers.handleToggledIssue)
+
+  app.on(['issues.closed', 'pull_request.closed'], handlers.handleToggledIssue)
+
+  app.on(['issues.labeled', 'pull_request.labeled'], handlers.handleLabelledIssue)
+
+}
