94c209b26e50733d7e2db8b335bd62b058aeebe8
AntoineDao <antoine@mallzee.com>
2019-09-14 23:30:45 +0800

    refactor(lib): rename folders and code files

---
+++ index.js
@@ -0,0 +1,16 @@
+const probot = require('./lib/probot');
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  app.log('project-manager was loaded!')
+
+  app.on(['issues.opened', 'pull_request.opened'], probot.handleToggledIssue)
+
+  app.on(['issues.closed', 'pull_request.closed'], probot.handleToggledIssue)
+
+  app.on(['issues.labeled', 'pull_request.labeled'], probot.handleLabelledIssue)
+
+}
